1ad5f08211316d83a2bb675001c283a77bbd131b
Fernando Nogueira <abert036@uottawa.ca>
2023-10-26 01:07:23 -0400

    wip

---
+++ src/lib/components/Logo.svelte
@@ -0,0 +1,18 @@
+<!-- Squircle -->
+<div
+	class="grid place-content-center aspect-square w-full h-full bg-primary-backdrop-token rounded-[25%]"
+>
+	<div class="aspect-square w-8 w-8 pick bg-primary-500"></div>
+</div>
+
+<style>
+	.pick {
+		--roundness: 80%;
+		border-top-left-radius: var(--roundness);
+		border-top-right-radius: var(--roundness) 100%;
+		border-bottom-right-radius: 5px;
+		border-bottom-left-radius: 100% var(--roundness);
+
+		transform: rotate(45deg);
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
 	import { audioContextStarted } from '$lib/mic/audioContext';
 	import { onMount } from 'svelte';
+	import { AppShell } from '@skeletonlabs/skeleton';
+	import { AppBar } from '@skeletonlabs/skeleton';
+	import { AppRail, AppRailTile, AppRailAnchor } from '@skeletonlabs/skeleton';
 	import '../app.postcss';
-
+	import Logo from '../lib/components/Logo.svelte';
+	let currentTile = 0;
 	async function initializeTone() {
 		await tone.start();
 		audioContextStarted.set(true);
@@ -30,26 +34,64 @@
 	</button>
 {/if}
 
-<div class="w-full h-screen flex flex-col">
-	<div class="navbar">
-		<div class="flex-1 gap-2">
-			<a class="btn btn-ghost normal-case text-xl" href="/">SStrings</a>
-			<a class="btn btn-ghost normal-case text-xl" href="/tuner">Tuner</a>
-			<a class="btn btn-ghost normal-case text-xl" href="/game">Game</a>
-			<a class="btn btn-ghost normal-case text-xl" href="/spectrogram">Spectrogram</a>
-		</div>
-		<div class="flex-none gap-2">
-			<a href="/settings" class="btn btn-outline">⚙️</a>
-
-			<!-- svelte-ignore a11y-label-has-associated-control -->
-			<div class="dropdown dropdown-end">
-				<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
-				<label tabindex="0" class="btn btn-ghost btn-circle avatar">
-					<div class="w-10 rounded-full bg-white" />
-				</label>
+<AppShell>
+	<svelte:fragment slot="header">
+		<AppBar>
+			<svelte:fragment slot="lead"></svelte:fragment>
+			<div class="flex-1 gap-2">
+				<a class="btn btn-ghost normal-case text-xl" href="/tuner">Tuner</a>
+				<a class="btn btn-ghost normal-case text-xl" href="/game">Game</a>
+				<a class="btn btn-ghost normal-case text-xl" href="/spectrogram">Spectrogram</a>
 			</div>
-		</div>
-	</div>
+			<svelte:fragment slot="trail">
+				<div class="flex-none gap-2">
+					<a href="/settings" class="w-full h-full absolute">⚙️</a>
 
+					<!-- <div class="dropdown dropdown-end">
+						<label tabindex="0" class="btn btn-ghost btn-circle avatar">
+							<div class="w-10 rounded-full bg-white" />
+						</label>
+					</div> -->
+				</div>
+			</svelte:fragment>
+		</AppBar>
+		<div class="navbar"></div>
+	</svelte:fragment>
+	<svelte:fragment slot="sidebarLeft">
+		<AppRail>
+			<svelte:fragment slot="lead">
+				<!-- <a class="btn btn-ghost normal-case text-xl" href="/">SStrings</a> -->
+				<AppRailAnchor href="/" class="">
+					<div class="p-1">
+						<Logo />
+					</div>
+					<!-- <img src="/logo.svg" alt="SStrings" class="w-10 h-10" /> -->
+				</AppRailAnchor>
+			</svelte:fragment>
+			<!-- --- -->
+			<AppRailTile bind:group={currentTile} name="tile-1" value={0} title="tile-1">
+				<svelte:fragment slot="lead">(icon)</svelte:fragment>
+				<span>Tile 1</span>
+			</AppRailTile>
+			<AppRailTile bind:group={currentTile} name="tile-2" value={1} title="tile-2">
+				<svelte:fragment slot="lead">(icon)</svelte:fragment>
+				<span>Tile 2</span>
+			</AppRailTile>
+			<AppRailTile bind:group={currentTile} name="tile-3" value={2} title="tile-3">
+				<svelte:fragment slot="lead">(icon)</svelte:fragment>
+				<span>Tile 3</span>
+			</AppRailTile>
+			<!-- --- -->
+			<svelte:fragment slot="trail">
+				<AppRailAnchor href="/" target="_blank" title="Account">(icon)</AppRailAnchor>
+			</svelte:fragment>
+		</AppRail>
+	</svelte:fragment>
+	<!-- (sidebarRight) -->
+	<!-- (pageHeader) -->
+	<!-- Router Slot -->
 	<slot />
-</div>
+	<!-- ---- / ---- -->
+	<svelte:fragment slot="pageFooter">Page Footer</svelte:fragment>
+	<!-- (footer) -->
+</AppShell>
